cfb811de64c0fc66dbef525de7484fde1ebd9bdd
Leah Welch <ArtistD2@MacBook-Pro.fios-router.home>
2020-01-18 12:15:55 -0500

    cleaned up JS

--- homework/01-16/script.js
+++ homework/01-16/script.js
@@ -29,8 +29,8 @@ function fetchData() {
             .append("line")
               .attr("x1", margin.left)
               .attr("x2", function(data) { return (data.value*5); })
-              .attr("y1", 100)
-              .attr("y2", 100)
+              .attr("y1", height/2)
+              .attr("y2", height/2)
               .attr("class", "myline")
               .attr("stroke", "#A7A7A7")
               .attr("stroke-width", 5);
@@ -40,136 +40,23 @@ function fetchData() {
         newLines.enter().append("line")
             .attr("x1", margin.left)
             .attr("x2", function(data) { return (data.value*5); })
-            .attr("y1", 100)
-            .attr("y2", 100)
+            .attr("y1", height/2)
+            .attr("y2", height/2)
         .merge(newLines)
             .transition()
                 .attr("x1", margin.left)
                 .attr("x2", function(data) { return (data.value*5); })
-                .attr("y1", 100)
-                .attr("y2", 100)
+                .attr("y1", height/2)
+                .attr("y2", height/2)
             .attr("stroke", "#A7A7A7")
             .attr("stroke-width", 5);
         
         newLines.exit()
             .transition()
             .remove();
-
-        /*var points = svg.selectAll("circle")
-            .data(data/*, function(d) { return d.country; })
-            .enter()
-            .append("circle")
-                .attr("cx", width/2)
-                .attr("cy", 350)
-                .attr("r", function(d) { return d.value; })
-                .attr("fill", "red"/*function(d) { return colorScale(d.continent); });
-
-        var newPoints = svg.selectAll("circle")
-            .data(data);
-
-        newPoints.enter().append("circle")
-            .attr("cx", width/2)
-            .attr("cy", 350)
-            .attr("r", function(d) { return d.value; })
-            .attr("fill", "red")
-        .merge(newPoints)
-        .transition()
-        .duration(1000)
-        .delay(250)
-        .attr("cx", width/2)
-        .attr("cy", 350)
-        .attr("r", function(d) { return d.value; })
-        .attr("fill", "red");
-        newPoints.exit()
-            .transition()
-            .duration(1000)
-            .delay(250)
-            .attr("r", 0)
-            .remove();*/
     });
 }
 
 fetchData();
 setInterval(fetchData, frequency);
 
-
-    /*var rScale = d3.scaleSqrt()
-        .domain([pop.min1957, pop.max1957])
-        .range([3, 25]);
-
-    var colorScale = d3.scaleOrdinal(d3.schemeCategory10);
-
-    var xAxis = svg.append("g")
-        .attr("class","axis")
-        .attr("transform", `translate(0,${height-margin.bottom})`)
-        .call(d3.axisBottom().scale(xScale));
-
-    var yAxis = svg.append("g")
-        .attr("class","axis")
-        .attr("transform", `translate(${margin.left},0)`)
-        .call(d3.axisLeft().scale(yScale));
-    //Drawing points using the 1957 filtered data set//
-    var points = svg.selectAll("circle")
-        .data(filtered_data1957, function(d) { return d.country; })
-        .enter()
-        .append("circle")
-            .attr("cx", function(d) { return xScale(d.lifeExp); })
-            .attr("cy", function(d) { return yScale(d.gdpPercap); })
-            .attr("r", function(d) { return rScale(d.pop); })
-            .attr("fill", function(d) { return colorScale(d.continent); });
-
-    var xAxisLabel = svg.append("text")
-        .attr("class","axisLabel")
-        .attr("x", width/2)
-        .attr("y", height-margin.bottom/2)
-        .text("Life Expectancy");
-
-    var yAxisLabel = svg.append("text")
-        .attr("class","axisLabel")
-        .attr("transform","rotate(-90)")
-        .attr("x",-height/2)
-        .attr("y",margin.left/2)
-        .text("GDP Per Capita");
-
-      // The data update //
-      //setting the new scales for 2007//
-      xScale.domain([lifeExp.min2007, lifeExp.max2007]);
-      yScale.domain([gdpPercap.min2007, gdpPercap.max2007]);
-      rScale.domain([pop.min2007, pop.max2007]);
-      //grabbing all circles and assigning the filtered data set for 2007//
-      var newPoints = svg.selectAll("circle")
-        .data(filtered_data2007, function(d) { return d.country; });
-      //drawing circles for that new dataset//
-      newPoints.enter().append("circle")
-        .attr("cx", function(d) { return xScale(d.lifeExp); })
-        .attr("cy", function(d) { return yScale(d.gdpPercap); })
-        .attr("r", function(d) { return rScale(d.pop); })
-        .attr("fill", function(d) { return colorScale(d.continent); })
-        //merge and transition of datapoints//
-      .merge(newPoints)
-        .transition()
-        .duration(1000)
-        .delay(250)
-        .attr("cx", function(d) { return xScale(d.lifeExp); })
-        .attr("cy", function(d) { return yScale(d.gdpPercap); })
-        .attr("r", function(d) { return rScale(d.pop); })
-        .attr("fill", function(d) { return colorScale(d.continent); });
-      //exit method for new data points, remove the points that are no longer in the set//
-      newPoints.exit()
-        .transition()
-        .duration(1000)
-        .delay(250)
-        .attr("r", 0)
-        .remove();
-      //transition the axes//
-      xAxis.transition()
-        .duration(1000)
-        .delay(250)
-        .call(d3.axisBottom().scale(xScale));
-
-      yAxis.transition()
-        .duration(1000)
-        .delay(250)
-        .call(d3.axisLeft().scale(yScale));
-
-});*/
