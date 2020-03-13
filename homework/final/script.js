cd80f9cbbc9b241efce831b87486e8dcf0cbd5a7
Leah Welch <ArtistD2@MacBook-Pro.fios-router.home>
2020-03-12 21:33:40 -0400

    loading data?

--- homework/final/script.js
+++ homework/final/script.js
@@ -5,7 +5,7 @@ d3.csv("./data/themoviedb_2019.csv", function(data) {
 
     data.forEach(function(d){
         d.year = d.year.substring(1); 
-        console.log(d.year);
+        //console.log(d.year);
     })
     
 
