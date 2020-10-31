221943bc25562b3a46e22b18aec160b821ed7ee5
LEAH WELCH <leahwelch@leahs-imac.myfiosgateway.com>
2020-10-31 15:34:40 -0400

    scrolling and eras

--- projects/wardrobe2/script.js
+++ projects/wardrobe2/script.js
@@ -3,19 +3,11 @@ var promises = [
     d3.csv("./data/eras.csv"), 
     d3.csv("./data/Wardrobe.csv")
 ];
-window.createGraphic = function(graphicSelector) {
     
-    var graphicEl = d3.select('.graphic')
-    var graphicVisEl = graphicEl.select('.graphic__vis')
+    var graphicEl = d3.select('#container')
+    var graphicVisEl = graphicEl.select('#graph')
     var graphicProseEl = graphicEl.select('.graphic__prose')
 
-    var sticky = graphicVisEl.offsetTop;
-
-    var viewHeight = document.querySelector(".graphic").clientHeight;
-    var sHeight = document.querySelector(".graphic__prose").scrollHeight;   
-    
-    console.log(viewHeight);
-    console.log(sHeight);
 Promise.all(promises).then(function(wardrobedata) {
 
     var eras = wardrobedata[0];
@@ -26,7 +18,7 @@ Promise.all(promises).then(function(wardrobedata) {
         era_start.push(eras[i].start);
     }
     console.log(era_start);
-    var width = document.querySelector(".graphic__vis").clientWidth;
+    var width = document.querySelector("#graph").clientWidth;
     var widthT = document.querySelector(".graphic__prose").clientWidth;
     var heightT = document.querySelector(".graphic__prose").clientHeight;
     var marginT = {top: 0, left: 10, right: 0, bottom: 0};
@@ -39,80 +31,30 @@ Promise.all(promises).then(function(wardrobedata) {
         .domain([1, 60])
         .range([0, heightT]);
     
-    var yAxisGenerator = d3.axisRight(yScaleT)
-        .tickSize(-14)
-        .ticks(60);
+    // var yAxisGenerator = d3.axisRight(yScaleT)
+    //     .tickSize(-14)
+    //     .ticks(60);
     
-    var yAxis = svgT.append("g")
-        .attr("class","axis")
-        .attr("transform", `translate(${marginT.left + 7},0)`)
-        .call(yAxisGenerator);
+    // var yAxis = svgT.append("g")
+    //     .attr("class","axis")
+    //     .attr("transform", `translate(${marginT.left + 7},0)`)
+    //     .call(yAxisGenerator);
     
-    yAxis.selectAll(".tick text")
-        .attr("class", "sideLabels")
-        .style("visibility", "hidden");
+    // yAxis.selectAll(".tick text")
+    //     .attr("class", "sideLabels")
+    //     .style("visibility", "hidden");
     
-    var lineT = svgT.append("line")
-        .attr("x1", marginT.left)
-        .attr("x2", marginT.left)
-        .attr("y1", function() {
-            return yScaleT(1);
-        })
-        .attr("y2", function() {
-            return yScaleT(60);
-        })
-        .attr("stroke", "#a08875");
-    
-
-    
-    // d3.select(".graphic__prose")
-    //     .selectAll(".trigger")
-    //     .data(eras)
-    //     .join("div")
-    //     .attr("class", function(d) {
-    //         if(d.start == 1 || d.end == 60) {
-    //             return "notTrigger";
-    //         } else {
-    //             return "trigger";
-    //         }
+    // var lineT = svgT.append("line")
+    //     .attr("x1", marginT.left)
+    //     .attr("x2", marginT.left)
+    //     .attr("y1", function() {
+    //         return yScaleT(1);
     //     })
-    //     .attr("id", function(d) {
-    //         return d.start;
+    //     .attr("y2", function() {
+    //         return yScaleT(60);
     //     })
-    //     .attr('data-step', function(d,i) {
-    //         if(d.start !== 1 && d.end !== 60) {
-    //             return i;
-    //         } 
-            
-    //     })
-        
-    //     .style("top", function(d) { return yScaleT(d.start) + 26 + "px"; }) //this is the problem right here
-    //     .style("left", d => width + widthT/2 - 14 + "px")
-    //     .style("visibility", function(d) {
-    //         if(d.start == 1 || d.end == 60) {
-    //             return "hidden";
-    //         } else {
-    //             return "visible";
-    //         }
-    //     })
-    //     .style(
-    //     "height", 8 + "px")
-    //     // d =>
-    //     //     (d.start >= d.end
-    //     //     ? 0
-    //     //     : yScaleT(d.end) - yScale(d.start)) +
-    //     //     2 +
-    //     //     "px"
-    //     // )
-    //     .html(
-    //         d => `<div class='timeline_date'>${d.start_year}</div>
-    //             <div class='timeline_loc'>${d.location}</div>
-    //         `
-        
-    //     );
-
-    //     var nyu = document.getElementById("3");
-    //     var nyuTop = nyu.getBoundingClientRect().top;
+    //     .attr("stroke", "#a08875");
+    
 
     console.log(wardrobe);
 
@@ -151,19 +93,37 @@ Promise.all(promises).then(function(wardrobedata) {
 
     
 
-    var width = document.querySelector(".graphic__vis").clientWidth;
-    var height = document.querySelector(".graphic__vis").clientHeight;
+    var width = document.querySelector("#graph").clientWidth;
+    var height = document.querySelector("#graph").clientHeight;
     var margin = {top: 0, left: 0, right: 0, bottom: 100};
     
-    var svg = d3.select(".graphic__vis")
+    var svg = d3.select("#graph")
         .append("svg")
         .attr("width", width)
         .attr("height", height);
 
-    // var annotation = d3.select(".graphic__vis").append("div").attr("class", "annotation")
-    //     .style("top", height/2)
-    //     .style("left", margin.left)
-    //     .style("visibility", "visible")
+    var patterns = [];
+    for(i = 0; i < 57; i++) {
+        svg.append("defs")
+            .append("pattern")
+                .attr("id", i+1)
+                .attr("x", 10)
+                .attr("y", 10)
+                .attr("width", 30)
+                .attr("height", 30)
+                .attr("patternUnits", "userSpaceOnUse")
+            .append("svg:image")
+                //.attr("xlink:href", "./pattern/rainbow.png")
+                .attr("xlink:href", `./pattern/${i + 1}.svg`)
+                .attr("width", 30)
+                .attr("height", 30);
+
+        patterns.push(`url(#${i+1})`)
+        }
+    console.log(patterns);
+
+       
+
 
     var xScale = d3.scaleBand()
         .domain(wardrobe.map(function(d) { return d.Category; }))
@@ -189,6 +149,7 @@ Promise.all(promises).then(function(wardrobedata) {
         .style("text-anchor", "start");
 
     var annotation = d3.select(".annotation")
+    var date_labels = d3.select(".date_labels")
           
     var topsG = svg.append("g").attr("class", "topsG")
 
@@ -201,7 +162,13 @@ Promise.all(promises).then(function(wardrobedata) {
         .attr("y", function(d) { return yScale(d.ypos); })
         .attr("width", 40)
         .attr("height", 10)
-        .attr("fill", function(d) { return d.Primary_Color; })
+        .attr("fill", function(d) { 
+            if(d.Pattern === "N") {
+                return d.Primary_Color; 
+            } else {
+                return patterns[d.Pattern_ID];
+            }
+            })
         .attr("rx", 2)								
 		.attr("ry", 2);
         //.style("opacity", 0.2);
@@ -287,83 +254,302 @@ Promise.all(promises).then(function(wardrobedata) {
                 .style("background-color", d.Primary_Color)
                 .html(d.Primary_Color)
             });
+
+    function sec_1() {
+        svg.selectAll("rect")
+            .attr("fill", function(d) {
+                if(d.Era === "Winnetka, IL") {
+                    return d.Primary_Color
+                } else {
+                    return "#ddd3ca";
+                }
+            })
+            .attr("opacity", function(d) {
+                if(d.Era === "Winnetka, IL") {
+                    return 1;
+                } else {
+                    return 0.3;
+                }
+            })
+            .attr("pointer-events", function(d) {
+                if(d.Era === "Winnetka, IL") {
+                    return "all";
+                }else{
+                    return "none";
+                }
+            });
+        date_labels.select(".start_date").html("before")
+        date_labels.select(".end_date").html("September, 2006")
+        date_labels.select(".era_name").html("Winnetka, IL")
+    }
+    function sec_2() {
+        svg.selectAll("rect")
+            .attr("fill", function(d) {
+                if(d.Era === "Freshman Year, Middlebury College" || d.Era == "Sophomore Year, Middlebury College") {
+                    return d.Primary_Color
+                } else {
+                    return "#ddd3ca";
+                }
+            })
+            .attr("opacity", function(d) {
+                if(d.Era === "Freshman Year, Middlebury College" || d.Era == "Sophomore Year, Middlebury College") {
+                    return 1;
+                } else {
+                    return 0.3;
+                }
+            })
+            .attr("pointer-events", function(d) {
+                if(d.Era === "Freshman Year, Middlebury College" || d.Era == "Sophomore Year, Middlebury College") {
+                    return "all";
+                }else{
+                    return "none";
+                }
+            });
+            date_labels.select(".start_date").html("September, 2007-")
+            date_labels.select(".end_date").html("September, 2009")
+            date_labels.select(".era_name").html("Middlebury College")
+    }
+    function sec_3() {
+        svg.selectAll("rect")
+            .attr("fill", function(d) {
+                if(d.Era === "Study Abroad, London") {
+                    return d.Primary_Color
+                } else {
+                    return "#ddd3ca";
+                }
+            })
+            .attr("opacity", function(d) {
+                if(d.Era === "Study Abroad, London") {
+                    return 1;
+                } else {
+                    return 0.3;
+                }
+            })
+            .attr("pointer-events", function(d) {
+                if(d.Era === "Study Abroad, London") {
+                    return "all";
+                }else{
+                    return "none";
+                }
+            });
+            date_labels.select(".start_date").html("September, 2009-")
+            date_labels.select(".end_date").html("September, 2010")
+            date_labels.select(".era_name").html("Year Abroad, London")
+    }
+    function sec_4() {
+        svg.selectAll("rect")
+            .attr("fill", function(d) {
+                if(d.Era === "Senior Year, Middlebury College") {
+                    return d.Primary_Color
+                } else {
+                    return "#ddd3ca";
+                }
+            })
+            .attr("opacity", function(d) {
+                if(d.Era === "Senior Year, Middlebury College") {
+                    return 1;
+                } else {
+                    return 0.3;
+                }
+            })
+            .attr("pointer-events", function(d) {
+                if(d.Era === "Senior Year, Middlebury College") {
+                    return "all";
+                }else{
+                    return "none";
+                }
+            });
+            date_labels.select(".start_date").html("September, 2010-")
+            date_labels.select(".end_date").html("September, 2011")
+            date_labels.select(".era_name").html("Middlebury College")
+    }
+    function sec_5() {
+        svg.selectAll("rect")
+            .attr("fill", function(d) {
+                if(d.Era === "Los Angeles") {
+                    return d.Primary_Color
+                } else {
+                    return "#ddd3ca";
+                }
+            })
+            .attr("opacity", function(d) {
+                if(d.Era === "Los Angeles") {
+                    return 1;
+                } else {
+                    return 0.3;
+                }
+            })
+            .attr("pointer-events", function(d) {
+                if(d.Era === "Los Angeles") {
+                    return "all";
+                }else{
+                    return "none";
+                }
+            });
+            date_labels.select(".start_date").html("June, 2011-")
+            date_labels.select(".end_date").html("June, 2014")
+            date_labels.select(".era_name").html("Los Angeles")
+    }
+    function sec_6() {
+        svg.selectAll("rect")
+            .attr("fill", function(d) {
+                if(d.Era === "Moved in with Steven, Nottingham") {
+                    return d.Primary_Color
+                } else {
+                    return "#ddd3ca";
+                }
+            })
+            .attr("opacity", function(d) {
+                if(d.Era === "Moved in with Steven, Nottingham") {
+                    return 1;
+                } else {
+                    return 0.3;
+                }
+            })
+            .attr("pointer-events", function(d) {
+                if(d.Era === "Moved in with Steven, Nottingham") {
+                    return "all";
+                }else{
+                    return "none";
+                }
+            });
+            date_labels.select(".start_date").html("June, 2014-")
+            date_labels.select(".end_date").html("August, 2014")
+            date_labels.select(".era_name").html("Nottingham, England")
+    }
+    function sec_7() {
+        svg.selectAll("rect")
+            .attr("fill", function(d) {
+                if(d.Era === "Hangzhou, China") {
+                    return d.Primary_Color
+                } else {
+                    return "#ddd3ca";
+                }
+            })
+            .attr("opacity", function(d) {
+                if(d.Era === "Hangzhou, China") {
+                    return 1;
+                } else {
+                    return 0.3;
+                }
+            })
+            .attr("pointer-events", function(d) {
+                if(d.Era === "Hangzhou, China") {
+                    return "all";
+                }else{
+                    return "none";
+                }
+            });
+            date_labels.select(".start_date").html("September, 2014-")
+            date_labels.select(".end_date").html("August, 2016")
+            date_labels.select(".era_name").html("Hangzhou, China")
+    }
+    function sec_8() {
+        svg.selectAll("rect")
+            .attr("fill", function(d) {
+                if(d.Era === "The Orme School, Mayer, AZ") {
+                    return d.Primary_Color
+                } else {
+                    return "#ddd3ca";
+                }
+            })
+            .attr("opacity", function(d) {
+                if(d.Era === "The Orme School, Mayer, AZ") {
+                    return 1;
+                } else {
+                    return 0.3;
+                }
+            })
+            .attr("pointer-events", function(d) {
+                if(d.Era === "The Orme School, Mayer, AZ") {
+                    return "all";
+                }else{
+                    return "none";
+                }
+            });
+            date_labels.select(".start_date").html("September, 2016-")
+            date_labels.select(".end_date").html("June, 2019")
+            date_labels.select(".era_name").html("The Orme School")
+    }
+    function sec_9() {
+        svg.selectAll("rect")
+            .attr("fill", function(d) {
+                if(d.Era === "Roslindale, MA") {
+                    return d.Primary_Color
+                } else {
+                    return "#ddd3ca";
+                }
+            })
+            .attr("opacity", function(d) {
+                if(d.Era === "Roslindale, MA") {
+                    return 1;
+                } else {
+                    return 0.3;
+                }
+            })
+            .attr("pointer-events", function(d) {
+                if(d.Era === "Roslindale, MA") {
+                    return "all";
+                }else{
+                    return "none";
+                }
+            });
+            date_labels.select(".start_date").html("June, 2019-")
+            date_labels.select(".end_date").html("June, 2020")
+            date_labels.select(".era_name").html("Roslindale, MA")
+    }
+    function sec_10() {
+        svg.selectAll("rect")
+            .attr("fill", function(d) {
+                if(d.Era === "Lawrence, MA") {
+                    return d.Primary_Color
+                } else {
+                    return "#ddd3ca";
+                }
+            })
+            .attr("opacity", function(d) {
+                if(d.Era === "Lawrence, MA") {
+                    return 1;
+                } else {
+                    return 0.3;
+                }
+            })
+            .attr("pointer-events", function(d) {
+                if(d.Era === "Lawrence, MA") {
+                    return "all";
+                }else{
+                    return "none";
+                }
+            });
+            date_labels.select(".start_date").html("June, 2020-")
+            date_labels.select(".end_date").html("present")
+            date_labels.select(".era_name").html("Lawrence, MA")
+    }
+    
+    var gs = d3.graphScroll()
+        .container(d3.select('#container'))
+        .graph(d3.selectAll('#graph'))
+        .sections(d3.selectAll('.flags > div'))
+        // .offset(height)
+        .eventId('uniqueId1')
+        .on('active', function(i) {
+
+        [
+            sec_1,
+            sec_2, 
+            sec_3,
+            sec_4,
+            sec_5,
+            sec_6, 
+            sec_7,
+            sec_8,
+            sec_9,
+            sec_10
+        ][i]();
+
+        });
                 
 
 
     
-
-
-        var steps = [
-            function step0() {
-                
-            },
-            function step1() {
-                console.log("yo");
-            },
-            function step2() {
-    
-            },
-            function step3() {
-    
-            },
-            function step4() {
-    
-            },
-            function step5() {
-    
-            },
-            function step6() {
-    
-            },
-            function step7() {
-    
-            },
-            function step8() {
-    
-            },
-            function step9() {
-                console.log("chart is updating");
-            },
-            function step10() {
-    
-            },
-            function step11() {
-    
-            }
-        ]
-    
-        // update our chart
-        function update(step) {
-            steps[step].call()
-        }
-        
-        function setupProse() {
-            var height = window.innerHeight * 0.5
-            graphicProseEl.selectAll('.trigger')
-                .style('height', height + 'px')
-        }
-        
-        function init() {
-            //setupProse()
-            update(0)
-        }
-        
-        init()
-        
-        return {
-            update: update,
-        }
-    
-
-
-
 });
-
-window.addEventListener("scroll", function() { 
-        
-    scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-        if (scrollTop > nyuTop) {
-            console.log("nyu");
-        }
-});
-}
