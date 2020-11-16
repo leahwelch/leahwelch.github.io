2c48d9f6c8479420ea90dceffeb7f9e26ba26838
LEAH WELCH <leahwelch@leahs-imac.myfiosgateway.com>
2020-11-16 15:13:31 -0500

    vintage fixes

--- projects/wardrobe2/script.js
+++ projects/wardrobe2/script.js
@@ -600,6 +600,57 @@ Promise.all(promises).then(function(wardrobedata) {
         .attr("rx", 2)								
         .attr("ry", 2);
 
+    const markerBoxWidth = 8;
+    const markerBoxHeight = 8;
+    const refX = markerBoxWidth / 2;
+    const refY = markerBoxHeight / 2;
+    const markerWidth = markerBoxWidth / 2;
+    const markerHeight = markerBoxHeight / 2;
+    const arrowPoints = [[0, 0], [0, 8], [8, 4]];
+
+    svgA.append("svg:defs").append("svg:marker")
+        .attr("id", "triangle")
+        .attr('viewBox', [0, 0, markerBoxWidth, markerBoxHeight])
+        .attr('refX', refX)
+        .attr('refY', refY)
+        .attr('markerWidth', markerBoxWidth)
+        .attr('markerHeight', markerBoxHeight)
+        .attr("orient", "auto-start-reverse")
+        .append("path")
+        .attr("d", d3.line()(arrowPoints))
+        .style("fill", "#a08875");
+
+    svgA.append("line")
+        .attr("x1", widthA-marginA.right-300)
+        .attr("x2", widthA-marginA.right-300)
+        .attr("y1", marginA.top)
+        .attr("y2", 200)
+        .attr("stroke", "#a08875")
+        .attr("marker-start", "url(#triangle)");
+
+    svgA.append("line")
+        .attr("x1", widthA-marginA.right-300)
+        .attr("x2", widthA-marginA.right-300)
+        .attr("y1", 600)
+        .attr("y2", heightA-marginA.bottom-10)
+        .attr("stroke", "#a08875")
+        .attr("marker-end", "url(#triangle)");
+
+    svgA.append("text")
+        .attr("x", 0)
+        .attr("y", 4)
+        .attr("transform", function(d){ return( "translate(" + (+widthA-marginA.right-300) + "," + (260) + ")rotate(-90)")})
+        .text("Newer")
+
+    svgA.append("text")
+        .attr("x", 0)
+        .attr("y", 4)
+        .attr("transform", function(d){ return( "translate(" + (+widthA-marginA.right-300) + "," + (580) + ")rotate(-90)")})
+        .text("Older")
+
+
+    
+
     
 
     // var xScaleVintage = d3.scaleLinear()
