c921c569902fa50e861341d9e66fa1eb0d8c20a2
LEAH WELCH <leahwelch@leahs-imac.myfiosgateway.com>
2021-04-27 20:57:00 -0400

    color cghange working

--- projects/earthlikeOrbits/sketch.js
+++ projects/earthlikeOrbits/sketch.js
@@ -1,14 +1,21 @@
 let movers = [];
+let lagranges = [];
 let l1;
+let l2;
+let l3;
+let l4;
+let l5;
 let sun;
 let sunX;
 let e;
 let earthX;
 let cnv;
 let labelColor;
+let fillColor;
 let nextButton;
 let prevButton;
 let dist;
+let slider;
 
 function centerCanvas() {
   let canvasX = (windowWidth - width) / 2;
@@ -21,10 +28,13 @@ function setup() {
     let canvasY;
 
     labelColor=255;
+    fillColor = color(8,8,45);
 
     nextButton = select("#next");
     prevButton = select("#prev");
-    nextButton.mousePressed(update1);
+    slider = select("#myRange");
+    fillColor=255;
+    
     // button.mousePressed(update1);
 
     cnv = createCanvas(800, 600);
@@ -45,8 +55,18 @@ function setup() {
     
     sun = new Attractor(sunX, height / 2, 3000,50, 249, 244, 189);
     e = new Attractor(earthX, height / 2, 1,10, 27, 244, 189);
+    // update1();
 
-    background(8,8,45);
+    l2 = new Lagrange(earthX - (dist/10), height/2);
+    l3 = new Lagrange(sunX - dist, height/2);
+    l4 = new Lagrange(earthX-(dist/2), height/2-sqrt(sq(dist) - sq(dist/2)));
+    l5 = new Lagrange(earthX-(dist/2), height/2+sqrt(sq(dist) - sq(dist/2)));
+    l1 = new Lagrange(earthX + (dist/10), height/2);
+
+    nextButton.mousePressed(update1);
+    
+
+    // background(8,8,45);
 }
 
 function draw() {
@@ -73,15 +93,18 @@ function draw() {
   ellipse(sunX - dist, height/2, 10, 10);
   ellipse(earthX-(dist/2), height/2+sqrt(sq(dist) - sq(dist/2)), 10, 10);
   ellipse(earthX-(dist/2), height/2-sqrt(sq(dist) - sq(dist/2)), 10, 10);
+  
+  l1.show();
+  l2.show();
+  l3.show();
+  l4.show();
+  l5.show();
 }
 
 function update1() {
-  labelColor = color(8,8,45);
+  // labelColor = color(8,8,45);
+  fillColor=color(8,8,45);
 }
-
-
-  
-
 
 class Attractor {
     constructor(x, y, m,r, c1, c2, c3) {
@@ -96,7 +119,7 @@ class Attractor {
     attract(mover) {
       let force = p5.Vector.sub(this.pos, mover.pos);
       let distanceSq = constrain(force.magSq(), 100, 1000);
-      let G = 0.01;
+      let G = slider.value()/4000;
       let strength = (G * (this.mass * mover.mass)) / distanceSq;
       force.setMag(strength);
       mover.applyForce(force);
@@ -137,6 +160,36 @@ class Attractor {
       stroke(255);
       strokeWeight(0);
       fill(this.c1, this.c2, this.c3);
+      ellipse(this.pos.x, this.pos.y, this.r * 2);
+    }
+  }
+
+  class Lagrange {
+    constructor(x, y) {
+      this.pos = createVector(x, y);
+      this.vel = createVector(0, 0);
+      this.acc = createVector(0, 0);
+      this.mass = 3;
+      this.r = sqrt(this.mass) * 2;
+      
+    }
+  
+    applyForce(force) {
+      let f = p5.Vector.div(force, this.mass);
+      this.acc.add(f);
+    }
+  
+    update() {
+      this.vel.add(this.acc);
+      this.pos.add(this.vel);
+      this.acc.set(0, 0);
+    }
+  
+    show() {
+      let c = fillColor;
+      stroke(255);
+      strokeWeight(0);
+      fill(c);
       ellipse(this.pos.x, this.pos.y, this.r * 2);
     }
   }
