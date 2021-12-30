152f73b01d78c3ac2c8fc7fe9cf7c8d0497b1723
LEAH WELCH <leahwelch@LEAHs-iMac.local>
2021-12-30 13:28:34 -0500

    making things

---
+++ projects/wearlog2/ideation/colors.js
@@ -0,0 +1,161 @@
+var width = document.querySelector("#chart").clientWidth;
+var height = document.querySelector("#chart").clientHeight;
+var margin = {top: 250, left: 250, right: 20, bottom: 50};
+
+console.log(width);
+console.log(height);
+
+var svg = d3.select("#chart")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+
+var promises = [
+    d3.json("./data/wearlog.json"), 
+    d3.csv("./data/wearlog.csv", parse)
+];
+
+Promise.all(promises).then(function(data) {
+    jsonData = data[0];
+    csvData = data[1]
+        // .filter(d => d.date > new Date("2021-04-30") && d.date < new Date("2021-06-01"));
+
+    console.log(csvData)
+    let colors = [];
+    for(let i = 0; i < csvData.length; i++) {
+        colors.push(csvData[i].hex1)
+        colors.push(csvData[i].hex2)
+        colors.push(csvData[i].hex3)
+        colors.push(csvData[i].hex4)
+        colors.push(csvData[i].hex5)
+    }
+
+    
+
+    let nested = d3.nest()
+        .key(function(d) { return d})
+        .rollup(function(v) { return v.length;})
+        .entries(colors)
+
+    let hslColors = []
+    nested.forEach((d) => {
+        d.hsl = HEXtoHSL(d.key)
+        hslColors.push({key: d.key, hsl: HEXtoHSL(d.key)})
+    })
+
+    console.log(hslColors)
+
+    const hueScale = d3.scaleLinear()
+        .domain([0,360])
+        .range([margin.left, width-margin.right])
+
+    const satScale = d3.scaleLinear()
+        .domain([0,100])
+        .range([height-margin.bottom, margin.top])
+
+    const yScale = d3.scaleLinear()
+        .domain([0,600])
+        .range([height-margin.bottom, margin.top])
+
+    let histogramValues = d3.histogram()
+        .value(function(d) {return d.hsl.h})
+        .domain(hueScale.domain())
+        .thresholds(hueScale.ticks(25))
+
+    let bins = histogramValues(hslColors);
+    console.log(bins)
+    
+    hslColors.forEach((g) => {
+        bins.forEach((bin) => {
+            bin.sort((a,b) => d3.ascending(a.hsl.l, b.hsl.l))
+            bin.forEach((d,i) => {
+                d.xPos = bin.x0;
+                d.yPos = i*7;
+                // d.yPos = ((i) * d.value);
+            })
+            // console.log(bin)
+            if(bin.key === g.key) {
+                hslColors.push({xPos: bin.xPos, yPos: bin.yPos})
+            }
+        })
+    })
+
+
+    svg.selectAll("rect")
+        .data(hslColors)
+        .enter()
+        .append("rect")
+        .attr("x", d => hueScale(d.xPos))
+        .attr("width", 30)
+        .attr("y", d => yScale(d.yPos))
+        .attr("height", 5)
+        .attr("fill", (d) => d.key)
+    
+    
+});
+
+function parse(d) {
+
+    return {
+        date: new Date(d.date),
+        description: (d.Brand + " ").concat((d.Description + " ")).concat(d.Sub_Category),
+        id: +d.garmentId,
+        group: d.group,
+        hex1: d.hex1,
+        hex2: d.hex2,
+        hex3: d.hex3,
+        hex4: d.hex4,
+        hex5: d.hex5
+    }
+    
+}
+
+function HEXtoHSL(hex) {
+    hex = hex.replace(/#/g, '');
+    if (hex.length === 3) {
+        hex = hex.split('').map(function (hex) {
+            return hex + hex;
+        }).join('');
+    }
+    var result = /^([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})[\da-z]{0,0}$/i.exec(hex);
+    if (!result) {
+        return null;
+    }
+    var r = parseInt(result[1], 16);
+    var g = parseInt(result[2], 16);
+    var b = parseInt(result[3], 16);
+    r /= 255, g /= 255, b /= 255;
+    var max = Math.max(r, g, b),
+        min = Math.min(r, g, b);
+    var h, s, l = (max + min) / 2;
+    if (max == min) {
+        h = s = 0;
+    } else {
+        var d = max - min;
+        s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+        switch (max) {
+        case r:
+            h = (g - b) / d + (g < b ? 6 : 0);
+            break;
+        case g:
+            h = (b - r) / d + 2;
+            break;
+        case b:
+            h = (r - g) / d + 4;
+            break;
+        }
+        h /= 6;
+    }
+    s = s * 100;
+    s = Math.round(s);
+    l = l * 100;
+    l = Math.round(l);
+    h = Math.round(360 * h);
+
+    return {
+        h: h,
+        s: s,
+        l: l
+    };
+}
---
+++ projects/wearlog2/ideation/dayStacks_v2.js
@@ -0,0 +1,228 @@
+
+
+var width = document.querySelector("#chart").clientWidth;
+var height = document.querySelector("#chart").clientHeight;
+var margin = {top: 50, left: 50, right: 50, bottom: 50};
+
+var svg = d3.select("#chart")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+
+d3.csv("./data/wearlog.csv", parse).then(function(data) {
+
+
+    let colors = [];
+    for(let i = 0; i < data.length; i++) {
+        colors.push({color: data[i].hex1, date: data[i].date, id: data[i].id, description: data[i].description})
+        colors.push({color: data[i].hex2, date: data[i].date, id: data[i].id, description: data[i].description})
+        colors.push({color: data[i].hex3, date: data[i].date, id: data[i].id, description: data[i].description})
+        colors.push({color: data[i].hex4, date: data[i].date, id: data[i].id, description: data[i].description})
+        colors.push({color: data[i].hex5, date: data[i].date, id: data[i].id, description: data[i].description})
+    }
+
+    let nested = d3.nest()
+        .key(function(d) { return d.color})
+        .rollup(function(v) { return v.length;})
+        .entries(colors)
+
+    nested.forEach((d) => {
+        d.hsl = HEXtoHSL(d.key)
+        d.hue = d.hsl.h
+    })
+
+    colors.forEach((d) => {
+        d.hsl = HEXtoHSL(d.color)
+        d.hue = d.hsl.h
+    })
+
+    nested.sort((a,b) => d3.ascending(a.hue,b.hue))
+    colors.sort((a,b) => d3.ascending(a.hue,b.hue))
+    
+
+    const hueScale = d3.scaleLinear()
+        .domain([0,360])
+        .range([margin.left, width-margin.right])
+
+    let histogramValues = d3.histogram()
+        .value(function(d) {return d.hue})
+        .domain(hueScale.domain())
+        .thresholds(hueScale.ticks(24))
+
+    let bins = histogramValues(nested);
+    let beeBins = histogramValues(colors)
+    console.log(beeBins)
+    
+    nested.forEach((g) => {
+        bins.forEach((bin) => {
+            bin.forEach((d,i) => {
+                d.bucket = bin.x0;
+            })
+            // console.log(bin)
+            if(bin.key === g.key) {
+                nested.push({bucket: bin.x0})
+            }
+        })
+    })
+    
+
+    colors.forEach((g) => {
+        beeBins.forEach((bin) => {
+            bin.forEach((d,i) => {
+                d.bucket = bin.x0;
+            })
+            if(bin.date === g.date) {
+                colors.push({bucket: bin.x0})
+            }
+        })
+    })
+    console.log(colors)
+
+    // let filtered = colors.filter(d => d.bucket === 20)
+    let filtered = colors.filter(d => d.date > new Date("2021-10-01") && d.date < new Date("2021-11-01"))
+
+    let dateNest = d3.nest()
+        .key(d=>d.date)
+        .rollup()
+        .entries(filtered)
+
+    dateNest.forEach((d)=> {
+        d.key = new Date(d.key)
+        let itemNest = d3.nest()
+            .key(p=>p.id)
+            .rollup()
+            .entries(d.values)
+        d.itemNest = itemNest;
+    })
+
+    console.log(dateNest)
+    
+
+    let filterNest = d3.nest()
+        .key(d=>d.id)
+        .rollup()
+        .entries(filtered)
+
+    console.log(filterNest)
+
+    let stackData = [];
+    // filterNest.forEach((d,i) => {
+    //     d.values.forEach((p) => {
+    //         stackData.push({
+    //             ypos: i,
+    //             color: p.color,
+    //             date: p.date
+    //         })
+    //     })
+    // })
+    dateNest.forEach((d) => {
+        d.itemNest.forEach((p,i)=> {
+            p.values.forEach((m)=> {
+                stackData.push({
+                    ypos: i*1.5,
+                    color: m.color,
+                    date: m.date
+                })
+            })
+        })
+    })
+    console.log(stackData)
+
+    const xScale = d3.scaleTime()
+        .range([margin.left, width-margin.right])
+        .domain([new Date("2021-10-01"), new Date("2021-11-15")])
+
+    const yScale = d3.scaleLinear()
+        .domain([-10, 45])
+        .range([height-margin.bottom, margin.top])
+
+     svg.selectAll("circle")
+        .data(stackData)
+        .enter()
+        .append("circle")
+        .attr("cx", function(d) { return xScale(d.date) + ((Math.random()-0.5)*10); })
+        .attr("cy", function(d) { return yScale(d.ypos) + ((Math.random()-0.5)*35); })
+        .attr("r", 8)
+        .attr("fill", d=>d.color)
+
+    // var xAxis = svg.append("g")
+    //     .attr("class", "axis")
+    //     .attr("transform", `translate(0, ${height-margin.bottom})`)
+    //     .call(d3.axisBottom().scale(xScale));
+
+    // var yAxis = svg.append("g")
+    //     .attr("class","axis")
+    //     .attr("transform", `translate(${margin.left},0)`)
+    //     .call(d3.axisLeft().scale(yScale));
+
+    
+        
+
+   
+});
+
+function parse(d) {
+
+    return {
+        date: new Date(d.date),
+        description: (d.Brand + " ").concat((d.Description + " ")).concat(d.Sub_Category),
+        id: +d.garmentId,
+        group: d.group,
+        hex1: d.hex1,
+        hex2: d.hex2,
+        hex3: d.hex3,
+        hex4: d.hex4,
+        hex5: d.hex5
+    }
+    
+}
+
+function HEXtoHSL(hex) {
+    hex = hex.replace(/#/g, '');
+    if (hex.length === 3) {
+        hex = hex.split('').map(function (hex) {
+            return hex + hex;
+        }).join('');
+    }
+    var result = /^([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})[\da-z]{0,0}$/i.exec(hex);
+    if (!result) {
+        return null;
+    }
+    var r = parseInt(result[1], 16);
+    var g = parseInt(result[2], 16);
+    var b = parseInt(result[3], 16);
+    r /= 255, g /= 255, b /= 255;
+    var max = Math.max(r, g, b),
+        min = Math.min(r, g, b);
+    var h, s, l = (max + min) / 2;
+    if (max == min) {
+        h = s = 0;
+    } else {
+        var d = max - min;
+        s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+        switch (max) {
+        case r:
+            h = (g - b) / d + (g < b ? 6 : 0);
+            break;
+        case g:
+            h = (b - r) / d + 2;
+            break;
+        case b:
+            h = (r - g) / d + 4;
+            break;
+        }
+        h /= 6;
+    }
+    s = s * 100;
+    s = Math.round(s);
+    l = l * 100;
+    l = Math.round(l);
+    h = Math.round(360 * h);
+
+    return {
+        h: h,
+        s: s,
+        l: l
+    };
+}
