5755ab7b240423720a10e622bbed71d441b82b6a
LEAH WELCH <leahwelch@LEAHs-iMac.local>
2022-02-11 15:08:26 -0500

    data updates!

--- projects/style-chromatics/separated.js
+++ projects/style-chromatics/separated.js
@@ -183,7 +183,7 @@ d3.csv("./data/wearlog.csv", parse).then(function(data) {
     let root = treemap(hierarchy);
 
     const xScale = d3.scaleLinear()
-        .domain([1,68])
+        .domain([1,70])
         .range([margin.left, width-margin.right])
 
     const dateScale = d3.scaleTime()
