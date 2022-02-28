9fabe438ffd6ac45132b862b5705626cc557a9f3
LEAH WELCH <leahwelch@LEAHs-iMac.local>
2022-02-28 15:20:01 -0500

    light styling updates

---
+++ projects/siting/data.js
@@ -0,0 +1,14 @@
+var d = [
+
+	{
+		name:"Data Set",
+		color:"blue",
+		skills:[
+			{axis:"A",value:1},
+			{axis:"B",value:10},
+			{axis:"C",value:6},
+			{axis:"D",value:2},
+			{axis:"E",value:7}
+		  ],
+	}
+];
--- projects/style-chromatics/separated.js
+++ projects/style-chromatics/separated.js
@@ -183,7 +183,7 @@ d3.csv("./data/wearlog.csv", parse).then(function(data) {
     let root = treemap(hierarchy);
 
     const xScale = d3.scaleLinear()
-        .domain([1,70])
+        .domain([1,72])
         .range([margin.left, width-margin.right])
 
     const dateScale = d3.scaleTime()
