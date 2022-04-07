8d2aba2a350d9ce31d7a4c022213f1595fc931e1
LEAH WELCH <leahwelch@LEAHs-iMac.local>
2022-04-07 12:49:06 -0400

    deleting weird files

---
+++ projects/radar/data.js
@@ -0,0 +1,14 @@
+var d = [
+
+	{
+		name:"Data Set",
+		color:"#d62728",
+		skills:[
+			{axis:"Node",value:1},
+			{axis:"D3",value:1},
+			{axis:"Javascript",value:2},
+			{axis:"HTML",value:3},
+			{axis:"CSS",value:3}
+		  ],
+	}
+];
